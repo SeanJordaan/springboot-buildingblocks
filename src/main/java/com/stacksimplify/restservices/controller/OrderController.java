adb1d8ada6085efb0e29cf712435b53f13946b91
Sean <jordaansean7@gmail.com>
2022-10-12 17:20:12 +0200

    Done with Section 6

---
+++ src/main/java/com/stacksimplify/restservices/controller/OrderController.java
@@ -0,0 +1,73 @@
+package com.stacksimplify.restservices.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.apache.catalina.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.stacksimplify.restservices.entities.Order;
+import com.stacksimplify.restservices.exceptions.UserNotFoundException;
+import com.stacksimplify.restservices.repositories.OrderRepository;
+import com.stacksimplify.restservices.repositories.UserRepository;
+
+@RestController
+@RequestMapping(value ="/users")
+public class OrderController {
+
+	@Autowired
+	private UserRepository userRepository;
+	@Autowired 
+	private OrderRepository orderRepository;
+	
+	//get All orders for the users 
+	
+	@GetMapping("/{userid}/orders")
+	public List<Order> getAllOrders(@PathVariable Long userid) throws UserNotFoundException{
+		
+		Optional<com.stacksimplify.restservices.entities.User> userOptional = userRepository.findById(userid);
+		if(!userOptional.isPresent())
+			throw new UserNotFoundException("User Not Found");
+		return userOptional.get().getOrders();
+	}
+	
+	// Create Order
+
+	@PostMapping("{userid}/orders")
+	public Order createOrder(@PathVariable Long userid, @RequestBody Order order) throws UserNotFoundException {
+		Optional<User> userOptional = Optional.empty();
+
+		if (!userOptional.isPresent())
+			throw new UserNotFoundException("User Not Found");
+
+		User user = userOptional.get();
+		order.setUser(user);
+		return orderRepository.save(order);
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
---
+++ src/main/java/com/stacksimplify/restservices/entities/Order.java
@@ -0,0 +1,55 @@
+package com.stacksimplify.restservices.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import org.apache.catalina.User;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+@Entity
+@Table(name ="orders")
+public class Order {
+	
+	@Id
+	@GeneratedValue
+	private Long orderid;
+	private String orderdescription;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JsonIgnore
+	private User user;
+
+	public Order() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Long getOrderid() {
+		return orderid;
+	}
+
+	public void setOrderid(Long orderid) {
+		this.orderid = orderid;
+	}
+
+	public String getOrderdescription() {
+		return orderdescription;
+	}
+
+	public void setOrderdescription(String orderdescription) {
+		this.orderdescription = orderdescription;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	
+	
+
+}
